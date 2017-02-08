1cf4e821f1f1772c39a125e98995091de0233cef
charliep1110 <charliep1110@comcast.net>
2017-02-07 20:49:06 -0500

    Uploading R files, .csv's and instructions

---
+++ helpers.R
@@ -0,0 +1,51 @@
+# helpers.R
+# Winter break part II
+
+# Load Packages, download if necessary with install.packages()
+library(shiny)
+library(XML)
+library(stringr)
+library(dplyr)
+library(ggplot2)
+library(RColorBrewer)
+library(scales) 
+
+# These four lines read in the sics500.csv file and store it as two data frames
+# The filter command removes companies that don't have an industry
+# SICSCOPY is identical to SICS
+SICS <- read.csv("Data/sics500.csv",header = T, stringsAsFactors = F)
+SICS <- filter(SICS,!Industry==".Security currently not part of a SICS industry")
+SICSCOPY<- read.csv("Data/sics500.csv", header = TRUE, stringsAsFactors = F)
+SICSCOPY <- filter(SICSCOPY,!Industry==".Security currently not part of a SICS industry")
+# SICSCOPY is slightly altered to make columns 3 & 4 factors, as opposed to characters
+SICSCOPY[,3] <- as.factor(SICS[,3])
+SICSCOPY[,4] <- as.factor(SICS[,4])
+# Loads the fundamentals500.csv into the df FUND
+FUND <- read.csv("Data/fundamentals500.csv",header = T, stringsAsFactors = F)
+# Change the name of the third column of FUND to 'Total Revenue'
+names(FUND)[3]<-"Total Revenue"
+# The next four lines load compensation.csv and change column names
+COMP <- read.csv("Data/compensation.csv",header = T, stringsAsFactors = F)
+names(COMP)[20]<-"CEO Pay Ratio"
+names(COMP)[21]<-"F/M Pay Ratio of Salaried Employees"
+names(COMP)[22]<-"F/M Pay Ratio of Hourly Employees"
+# This line trims FUND to only include the 1-3 columns, removing the extras
+FUND <- FUND[,c(1:3)]
+# Fund includes info from 3 years of financial data, this filters only the 2015 data
+FUND <- filter(FUND,Year==2015)
+# This trims the COMP frame similar to how the FUND frame was
+COMP <- COMP[,c(2,20,21,22)]
+# TheTable is a data frame that is a composition of the SICS, FUND, and COMP data frames
+TheTable <- left_join(SICS,FUND,"Ticker") %>% left_join(COMP,"Ticker")
+# Once again, a copy is made for future use. Note below, TableCopy undergoes format changes while TheTable doesn't
+TableCOPY <- TheTable
+# Next four lines help format the table created in server.R
+# This line removes decimals from the CEO pay ratio column
+TableCOPY$`CEO Pay Ratio` <- floor(TableCOPY$`CEO Pay Ratio`)
+# The next two lines trim the last nine decimal places off the F/M ratio columns
+TableCOPY$`F/M Pay Ratio of Salaried Employees` <- sub(".........$", "", TableCOPY$`F/M Pay Ratio of Salaried Employees`)
+TableCOPY$`F/M Pay Ratio of Hourly Employees` <- sub(".........$", "", TableCOPY$`F/M Pay Ratio of Hourly Employees`)
+# This line changes the revenue numbers into dollar formatted nubmers ($1,000,000.00 vs. 1000000.0000)
+TableCOPY$`Total Revenue` <- dollar_format()(TableCOPY$`Total Revenue`)
+# Metric is a simple variable to be used in one column of the table
+Metric <- c('2015 Revenue','CEO Pay Ratio','Gender Pay Ratio (salaried)','Gender Pay Ratio (hourly)')
---
+++ server.R
@@ -0,0 +1,91 @@
+# server.R
+# winter break assignment part 2
+
+# load packages (download using "install.packages" function if not yet installed)
+library(shiny)
+library(XML)
+library(stringr)
+library(dplyr)
+library(ggplot2)
+library(RColorBrewer)
+library(scales)
+
+# Source file called helpers.R - should be in the same working directory as the app
+source("helpers.R")
+
+# This line is the structure of a server.R file. Always included, copy and paste from previous apps if you can't remember it
+shinyServer(function(input, output) {
+## The following renderText lines use the 'which' function to filter SICS
+## to only display company info for whichever company the user types in (by their ticker)
+## This is the "Quick Hitters" section of the UI
+  output$name <- renderText({
+    ## This, and all following, 'toupper' lines change the user's input to all CAPS, 
+    ## and store it as the variable TheTicker. This line must be repeated inside of every render___ call.
+    TheTicker <- toupper(input$text)
+    # More detail added to the above explanation: the which command and logical expression find where/what location
+    # TheTicker is equal to the ticker of a company in the SICS data frame. This location is a number.
+    # Then, SICS[], returns the whole row corresponding to that location. [2] pulls the 2nd entry, 
+    # the name, from the row of data in SICS - open SICS to see this. as. character makes it a character string.
+    ## In summary, this command finds the company its looking for, and returns it's name as a string.
+    as.character(SICS[which(SICS$Ticker == TheTicker),][2])
+  })
+  output$sector <- renderText({
+    TheTicker <- toupper(input$text)
+    # This line works the same way as the one previous except [3] pulls the 3rd entry, sector name, instead of company name
+    as.character(SICS[which(SICS$Ticker == TheTicker),][3])
+  })
+  output$industry <- renderText({
+    TheTicker <- toupper(input$text)
+    # Again, this works the same, but [4] pulls the industry name...
+    as.character(SICS[which(SICS$Ticker == TheTicker),][4])
+  })
+  output$number <- renderText({
+    TheTicker <- toupper(input$text)
+    # These two lines both use a which command, one after the other. In brief, the first identifies the industry of the company
+    # that the user types in and stores this industry in the variable called 'industry'.
+    # Then, the second line takes that industry, goes back to the SICS data frame and pulls out all of the companies
+    # in said industry. It stores the information/rows of data about all the companies in the said industry into the variable called
+    # industrycompanies
+    industry <- as.character(SICS[which(SICS$Ticker == TheTicker),][4])
+    industrycompanies <- SICSCOPY[which(SICSCOPY$Industry == industry),][,1]
+    # Lastly, this returns the length of 'industrycompanies' aka. the number of companies in the industry
+    length(industrycompanies)
+  })
+## This is where the table is created:
+  output$table <- renderTable({
+    TheTicker <- toupper(input$text)
+    # TableCOPY is a dataframe created in helpers.R and is a refined conjoinment of sics500, compensation, and fundamentals500
+    # The next four which commands work very similarly to the ones above but are now searching through TableCOPY
+    # The commands return the 6,7,8 & 9th value of TableCOPY, which are 2015 Revenue, CEO Pay Ratio,
+    # F/M pay ratio among salaried employees, and F/M ratio of hourly workers, respectively
+    # The four strings are stores in the variable 'Value'
+    # Value will be one of three columns put together to form the whole table
+    Value <- c(as.character(TableCOPY[which(TableCOPY$Ticker == TheTicker),][6]),
+               as.character(TableCOPY[which(TableCOPY$Ticker == TheTicker),][7]),
+               as.character(TableCOPY[which(TableCOPY$Ticker == TheTicker),][8]),
+               as.character(TableCOPY[which(TableCOPY$Ticker == TheTicker),][9]))
+    # industrycompanies is a matrix created by filtering TheTable by only companies in the same industry
+    # as the user-selected company - using the same/a similar which command as before
+    industrycompanies <- TheTable[which(TheTable$Industry == as.character(SICS[which(SICS$Ticker == TheTicker),][4])),]
+    # The following four rank functions assign a rank to each company in the industrycompanies matrix
+    # This is completed in four steps, one for each column to be ranked in industrycompanies, 
+    # the results stores in their own variable.
+    # At the moment, 'rank' gives the lowest number a rank of 1
+    RevRank <- rank(industrycompanies[,6])
+    CEORank <- rank(industrycompanies[,7])
+    SalRank <- rank(industrycompanies[,8])
+    HrlyRank <- rank(industrycompanies[,9])
+    # The final table requires just the rank of the company of interest, not all the companies in the industrycompanies matrix
+    # Thus this line identifies where in the industrycompanies the company is located, so we can print out
+    # the desired ranking
+    Position <- which(industrycompanies$Ticker==TheTicker)
+    # This line stores the four rankings as one column to be put into the final table
+    TempRank <- c(RevRank[Position],CEORank[Position],SalRank[Position],HrlyRank[Position])
+    # The rank function assigns the lowest value the rank of 1... This line reverses it,
+    # giving the highest number a rank of 1.
+    Rank <- length(RevRank)-TempRank+1
+    # Put the three columns together and BAM! you got a table
+    # Disclaimer: The Metric column was created in Helpers.R
+    BeautifulTable <- data_frame(Metric,Value,Rank)
+  })
+})

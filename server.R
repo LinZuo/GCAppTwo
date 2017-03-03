cc4184cb65d4f071a62309177decf668977a78ed
Charlie "Swaggy" Pearlman <charliep1110@Charlie-Pearlmans-MacBook-Pro.local>
2017-03-03 02:16:27 -0500

    Combined the 2 Apps!!

--- server.R
+++ server.R
@@ -1,6 +1,5 @@
-#THis is a test
 # server.R
-# winter break assignment part 2
+# Combined App!
 
 # load packages (download using "install.packages" function if not yet installed)
 library(shiny)
@@ -11,53 +10,116 @@ library(ggplot2)
 library(RColorBrewer)
 library(scales)
 
-#sd
-
 
 # Source file called helpers.R - should be in the same working directory as the app
 source("helpers.R")
 
 # This line is the structure of a server.R file. Always included, copy and paste from previous apps if you can't remember it
-shinyServer(function(input, output) {
+shinyServer(function(input, output, session) {
+  ### APP 1 ###
+  # This output creates the dropdown menu that appears in the sidebar
+  # This is created in server.R so that we only have to source helpers.R once,
+  # and not every timet he app is used
+  output$select <- renderUI({
+    selectInput("industry", label = h5("Select an industry"),
+                choices = levels(SICSCOPY$Industry), selected = levels(SICSCOPY$Industry)[63])
+  })
+  # Prints a copy of TheTable filtered by whatever industry the user selects
+  output$table <- renderTable({
+    TheTable <- filter(TableCOPY,Industry==as.character(input$industry))
+  })
+  # Displays either a histogram or a scatter plot - whichever the user selects - 
+  # of just the firms in the industry that the user selects
+  output$SwitchPlot <- renderPlot({
+    # First, both the histogram and scatter plot are created and stored in separate variables
+    # Since these plots only depict the selected industry, the "data =" and "aes" arguments
+    # include a filter command to filter TheTable by industry
+    Mustard <- ggplot(data=filter(TableCOPY,TheTable$Industry==as.character(input$industry)), aes(x = `Total Revenue`, y = `CEO Pay Ratio`, label = Company)) +
+      geom_point(aes(color = "red")) +
+      labs(title="SCATTA PLOT") +
+      labs(x="Total Revenue", y="CEO Pay Ratio") +
+      geom_text(size = 2, vjust = -0.75, fontface = "bold") +
+      geom_smooth(method=lm)
+    CatchUP <- ggplot(data=filter(TheTable,TheTable$Industry==as.character(input$industry)), aes(filter(TheTable,TheTable$Industry==as.character(input$industry))$`CEO Pay Ratio`)) +
+      geom_histogram(breaks=seq(0,700, by = 25),col="red",aes(fill=..count..)) +
+      labs(title = "Histogramsss for CEO Pay Ratio") +
+      labs(x = "Firms",y = "Count")
+    # This if structure tells R which plot to display on the UI based on which one the user picks
+    # The if tree looks at the value of the radio buttons, the first button corresponds to 
+    # the histogram, thus if input$radio==1, the tree returns the histogram (aka CatchUP), 
+    # If the second button is selected, then Mustard is returned
+    if (input$radio==1) {
+      CatchUP
+    } else {
+      Mustard
+    }
+  })
+  # Simple renderPlot function, using the ggplot of a histogram from past exercises
+  # This histogram is of all the firms in all industries
+  output$FullPlot <- renderPlot({
+    ggplot(data=TheTable, aes(TheTable$`CEO Pay Ratio`)) +
+      geom_histogram(breaks=seq(0,700, by = 25),col="red",aes(fill=..count..)) +
+      labs(title = "Histogram for CEO Pay Ratio of All Firms") +
+      labs(x = "Firms",y = "Count")
+  })
+  ## These two following sections enable the data download feature
+  # the reactive switch statement assigns datasetInput to be whichever matrix/dataframe
+  # the user chooses from the drop-down menu
+  datasetInput <- reactive({
+    switch(input$dataset,
+           "Sics" = SICS,
+           "Fundamentals" = FUND,
+           "Compensation" = COMP)
+  })
+  # This section I copied straight from the shiny gallery.
+  # The first part creates the filename, and the last part creates the data file to be downloaded
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      paste(input$dataset, '.csv', sep='')
+    },
+    content = function(file) {
+      write.csv(datasetInput(), file)
+    }
+  )
+  ### APP 2 ###
+  # This line uses the action button. The action button stores its value as input$submit,
+  # whenever the user changes the value of the text, and presses submit, the second argument runs:
+  # it stores the ticker symbol as a character in upper case letters to "submitTicker"
+  submitTicker <- eventReactive(input$submit, {
+    as.character(toupper(input$text))
+  })
 ## The following renderText lines use the 'which' function to filter SICS
 ## to only display company info for whichever company the user types in (by their ticker)
 ## This is the "Quick Hitters" section of the UI
   output$name <- renderText({
-    ## This, and all following, 'toupper' lines change the user's input to all CAPS, 
-    ## and store it as the variable TheTicker. This line must be repeated inside of every render___ call.
-    TheTicker <- toupper(input$text)
     # More detail added to the above explanation: the which command and logical expression find where/what location
     # TheTicker is equal to the ticker of a company in the SICS data frame. This location is a number.
     # Then, SICS[], returns the whole row corresponding to that location. [2] pulls the 2nd entry, 
     # the name, from the row of data in SICS - open SICS to see this. as. character makes it a character string.
     ## In summary, this command finds the company its looking for, and returns it's name as a string.
-    as.character(SICS[which(SICS$Ticker == TheTicker),][2])
+    as.character(SICS[which(SICS$Ticker == submitTicker()),][2])
   })
   output$sector <- renderText({
-    TheTicker <- toupper(input$text)
     # This line works the same way as the one previous except [3] pulls the 3rd entry, sector name, instead of company name
-    as.character(SICS[which(SICS$Ticker == TheTicker),][3])
+    as.character(SICS[which(SICS$Ticker == submitTicker()),][3])
   })
   output$industry <- renderText({
-    TheTicker <- toupper(input$text)
     # Again, this works the same, but [4] pulls the industry name...
-    as.character(SICS[which(SICS$Ticker == TheTicker),][4])
+    as.character(SICS[which(SICS$Ticker == submitTicker()),][4])
   })
   output$number <- renderText({
-    TheTicker <- toupper(input$text)
     # These two lines both use a which command, one after the other. In brief, the first identifies the industry of the company
     # that the user types in and stores this industry in the variable called 'industry'.
     # Then, the second line takes that industry, goes back to the SICS data frame and pulls out all of the companies
     # in said industry. It stores the information/rows of data about all the companies in the said industry into the variable called
     # industrycompanies
-    industry <- as.character(SICS[which(SICS$Ticker == TheTicker),][4])
+    industry <- as.character(SICS[which(SICS$Ticker == submitTicker()),][4])
     industrycompanies <- SICSCOPY[which(SICSCOPY$Industry == industry),][,1]
     # Lastly, this returns the length of 'industrycompanies' aka. the number of companies in the industry
     length(industrycompanies)
   })
 ## This is where the table is created:
-  output$table <- renderTable({
-    TheTicker <- toupper(input$text)
+  output$tablerank <- renderTable({
     # vector indicating the polarity of the metrics contained in the table
     Polarity <- c("+", "-", "+", "+")
     # TableCOPY is a dataframe created in helpers.R and is a refined conjoinment of sics500, compensation, and fundamentals500
@@ -66,13 +128,13 @@ shinyServer(function(input, output) {
     # F/M pay ratio among salaried employees, and F/M ratio of hourly workers, respectively
     # The four strings are stores in the variable 'Value'
     # Value will be one of three columns put together to form the whole table
-    Value <- c(as.character(TableCOPY[which(TableCOPY$Ticker == TheTicker),][6]),
-               as.character(as.integer(TableCOPY[which(TableCOPY$Ticker == TheTicker),][7])),
-               as.character(round(as.numeric(TableCOPY[which(TableCOPY$Ticker == TheTicker),][8]), 2)),
-               as.character(round(as.numeric(TableCOPY[which(TableCOPY$Ticker == TheTicker),][9]), 2)))
+    Value <- c(as.character(TableCOPY[which(TableCOPY$Ticker == submitTicker()),][6]),
+               as.character(as.integer(TableCOPY[which(TableCOPY$Ticker == submitTicker()),][7])),
+               as.character(round(as.numeric(TableCOPY[which(TableCOPY$Ticker == submitTicker()),][8]), 2)),
+               as.character(round(as.numeric(TableCOPY[which(TableCOPY$Ticker == submitTicker()),][9]), 2)))
     # industrycompanies is a matrix created by filtering TheTable by only companies in the same industry
     # as the user-selected company - using the same/a similar which command as before
-    industrycompanies <- TheTable[which(TheTable$Industry == as.character(SICS[which(SICS$Ticker == TheTicker),][4])),]
+    industrycompanies <- TheTable[which(TheTable$Industry == as.character(SICS[which(SICS$Ticker == submitTicker()),][4])),]
     # The following four rank functions assign a rank to each company in the industrycompanies matrix
     # This is completed in four steps, one for each column to be ranked in industrycompanies, 
     # the results stores in their own variable.
@@ -83,7 +145,7 @@ shinyServer(function(input, output) {
     # The final table requires just the rank of the company of interest, not all the companies in the industrycompanies matrix
     # Thus this line identifies where in the industrycompanies the company is located, so we can print out
     # the desired ranking
-    Position <- which(industrycompanies$Ticker==TheTicker)
+    Position <- which(industrycompanies$Ticker==submitTicker())
     # This line stores the four rankings as one column to be put into the final table
     Rank <- as.integer(c(RevRank[Position],CEORank[Position],SalRank[Position],HrlyRank[Position]))
     # Put the three columns together and BAM! you got a table
--- ui.R
+++ ui.R
@@ -1,36 +1,79 @@
 # ui.R
-# winter break assignment part 2
+# Combined App!
 
-shinyUI(fluidPage(
-  titlePanel("S&P 500 Companies"
-  ),
-  
-  sidebarLayout(
-    sidebarPanel(
-      # Text input, prompts user to type ticker
-      textInput("text", label = h5("Please enter ticker symbol:"), value = "AAPL"),
-      # Insert the link to look up ticker symbol
-      a(href="https://www.bloomberg.com/markets/symbolsearch", "Bloomberg's ticker lookup", target = "_blank"),
-      # Now includes a sumbit button
-      submitButton("Submit")
-    ),
-    
-    mainPanel(
-      h3("Company Overview"),
-      br(),
-      h5("Name:"),
-      textOutput("name"),
-      h5("Sector:"),
-      textOutput("sector"),
-      h5("Industry:"),
-      textOutput("industry"),
-      h5("Number of Companies in the Industry:"),
-      textOutput("number"),
-      br(),
-      ## Table with rankings printed
-      h3("Data Table"),
-      br(),
-      tableOutput("table")
-    )
-  )
+shinyUI(navbarPage("Navbar!",
+tabPanel("Industry View",
+         titlePanel("S&P 500 Companies"
+         ),
+         
+         sidebarLayout(
+           sidebarPanel(
+             # This uiOutput command prints out user interface created in the server.R file.
+             # It is created in server since there are reactive components to it.
+             uiOutput("select"),
+             # selectInput("industry", label = h5("Select an industry"), 
+             #             choices = levels(SICSCOPY$Industry), 
+             #             selected = 1),
+             # I copy and pasted all of these widgets from the shiny gallery
+             # The only one that required some specialization was the selectInput
+             # I wanted a dropdown with all the industries, and didn't want to type out 80 something names
+             # The "choices =" argument allows me to put in all the levels of the the SICS$Industry vector
+             # Reminder, the levels function returns the unique factors found in that vector
+             # selectInput("industry", label = h5("Select an industry"), 
+             #             choices = levels(SICS$Industry), 
+             #             selected = 1),
+             radioButtons("radio", label = h5("Select a type of plot to display"),
+                          choices = list("histogram of CEO pay ratio" = 1, 
+                                         "scatter plot of CEO pay ratio vs. total revenue" = 2), 
+                          selected = 1),
+             helpText("Download the data used in the creation of this page"),
+             selectInput("dataset", "Choose a dataset to download:",
+                         choices = c("Sics", "Fundamentals", "Compensation")),
+             downloadButton('downloadData', 'Download')
+           ),
+           
+           mainPanel(
+             # Simple outputs
+             h2("Industry Overview:"),
+             tableOutput('table'),
+             plotOutput("SwitchPlot"),
+             plotOutput("FullPlot")
+           )
+         )
+         ),
+tabPanel("Company View",
+         titlePanel("S&P 500 Companies"
+         ),
+         
+         sidebarLayout(
+           sidebarPanel(
+             # Text input, prompts user to type ticker
+             textInput("text", label = h5("Please enter ticker symbol:"), value = NULL),
+             # Now includes a sumbit button
+             actionButton("submit", "Fetch!"),
+             br(),
+             br(),
+             # Insert the link to look up ticker symbol
+             a(href="https://www.bloomberg.com/markets/symbolsearch", "Bloomberg's ticker lookup", target = "_blank")
+           ),
+           
+           mainPanel(
+             h3("Company Overview"),
+             br(),
+             h5("Name:"),
+             textOutput("name"),
+             h5("Sector:"),
+             textOutput("sector"),
+             h5("Industry:"),
+             textOutput("industry"),
+             h5("Number of Companies in the Industry:"),
+             textOutput("number"),
+             br(),
+             ## Table with rankings printed
+             h3("Data Table"),
+             br(),
+             tableOutput("tablerank")
+           )
+         )
+)
 ))
